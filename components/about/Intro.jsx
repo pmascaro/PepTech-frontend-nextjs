e7ab0aa857c37c00d13964db4b39c4658275e50b
Pep Marti Mascaró Monserrat <60947890+pmascaro@users.noreply.github.com>
2023-10-18 01:14:36 +0100

    Minor changes

--- components/about/Intro.jsx
+++ components/about/Intro.jsx
@@ -8,16 +8,16 @@ const Intro = () => {
     text: (
       <>
         <p>
-        Hello! I'm Pep. For me, 'Start with <i>WHY</i>' is more than just a phrase; 
-        it's the compass guiding everything I do. 
-        Throughout understanding people's stories and needs, my mission is to make life better. 
-        Beyond just numbers or algorithms, I'm passionate about finding solutions to real challenges, 
+        Hello! I&apos;Sm Pep. For me, &apos;Start with <i>WHY</i>&apos; is more than just a phrase; 
+        it&apos;Ss the compass guiding all I do. 
+        My mission is to enhance our lives throughout understanding people&apos;Ss stories and needs.
+        Beyond just numbers or algorithms, I&apos;m passionate about finding solutions to real challenges, 
         often leveraging the power of AI.
         </p>
         <p>
         While I emphasise collective impact and the importance of teams, this website is a personal canvas. 
-        It's where I showcase my work, share my solutions, and reflect on my experiences. 
-        My vision is that while individual efforts can shine, it's the impact we have on others 
+        It&apos;s where I showcase my work, share my solutions, and reflect on my experiences. 
+        My vision is that while individual efforts can shine, it&apos;s the impact we have on others 
         that truly defines us. So I want to welcome you to my space, where I bridge the gap between technology 
         and human narratives, one solution at a time.
         </p>
